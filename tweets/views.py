4e662ccdd07e312f45214390362ffc94d24ac1fe
RupakBiswas-2304 <75058161+RupakBiswas-2304@users.noreply.github.com>
2021-05-14 00:41:24 +0530

    restart

---
+++ tweets/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse,Http404,JsonResponse
+import random
+from .models import Tweet
+from .form import Tweet, TweetForm
+
+# Create your views h
+def home_view(request, *args, **kwargs):
+    return render(request,"pages/home.html", context={} , status = 200)
+
+def tweet_creat_view(request,*args,**kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request,"components/forms.html",context={"form":form})
+
+
+def tweet_list_view(request, *args , **kwargs):
+    qs = Tweet.objects.all()
+    tweet_list = [{"id":x.id, "content":x.content,"Likes": random.randint(0,125)} for x in qs]
+    data = {
+        "isUser":False,
+        "response": tweet_list,
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request, tweet_id,*args,**kwargs):
+    #rest API view
+    data = {
+        "id": tweet_id,
+    }
+    try:
+        obj = Tweet.objects.get(id = tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status = status)
+    # return HttpResponse(f"<h1> Hello {tweet_id} -{obj.content}</h1>")
