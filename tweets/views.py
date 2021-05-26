a7cad86bc9a6c18654d0f05fe9dd28ecdb755171
RupakBiswas-2304 <75058161+RupakBiswas-2304@users.noreply.github.com>
2021-05-26 14:37:04 +0530

    Rendering Error msg

--- tweets/views.py
+++ tweets/views.py
@@ -19,17 +19,20 @@ def tweet_creat_view(request,*args,**kwargs):
         obj = form.save(commit=False)
         obj.save()
         if request.is_ajax():
-            return JsonResponse({}, status = 201) #201 == created items 
+            return JsonResponse(obj.serialize(), status = 201) #201 == created items 
 
         if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors , status = 400)
     return render(request,"components/forms.html",context={"form":form})
 
 
 def tweet_list_view(request, *args , **kwargs):
     qs = Tweet.objects.all()
-    tweet_list = [{"id":x.id, "content":x.content,"Likes": random.randint(0,125)} for x in qs]
+    tweet_list = [x.serialize() for x in qs]
     data = {
         "isUser":False,
         "response": tweet_list,
