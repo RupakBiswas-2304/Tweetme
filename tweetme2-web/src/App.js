bb1cd62e5201999b3112c0b49adf080450809b72
RupakBiswas-2304 <75058161+RupakBiswas-2304@users.noreply.github.com>
2021-05-30 20:03:36 +0530

    react(node) + django(api)

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -2,16 +2,38 @@ import logo from './logo.svg';
 import React,{useEffect, useState} from 'react';
 import './App.css';
 
-
+function loadTweets(callback){
+  const xhr = new XMLHttpRequest()
+  const method = 'GET'//POST
+  const url = "http://localhost:8000/api/tweets"
+  const responseType = "json"
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function () {
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror = function(e){
+    console.log(e)
+    callback({"message":"The request was an error"},400)
+  }
+  xhr.send()
+}
 
 function App() {
   const [tweets, setTweets] = useState([])
-  // const performLookup = () =>{
-
-  // }
   useEffect(() => {
-    const tweetItems = [{"content": 123},{"content": "hello world"}]
-    setTweets(tweetItems)
+    const myCallback = (response, status) =>{
+      console.log(response, status)
+      if (status === 201){
+        setTweets(response)
+      }
+      else{
+        console.log("there was an error")
+      }
+      
+    }
+    loadTweets(myCallback)
+
   },[])
 
 
